44d1f071a637d36754a9a6722a8947c6313a592e
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2020-11-19 11:44:46 +0100

    test

--- main.c
+++ main.c
@@ -5,38 +5,128 @@
 
 char* getCurrentTime(void);
 int readFile(void);
+double getEvCharging(double evArray[], int evArrayLen, double pro_ex, int* numOfEvCharging, double limit);
+void generateEvArray(double evArray[], int evArrayLen);
+int cmpFun(const void *ep1, const void *ep2);
+void printArray(double array[], int arrayLen);
 
-int main (void)
-{
-    system("APIGetData\\GetElectricityProductionAPI.exe");
+int main (void) {
+    srand(10);
+
+    char yn;
+    printf("Do you want to get data? Type \"y\" for yes and \"n\" for no: ");
+    scanf(" %c", &yn);
+    if (yn == 'y') {
+        system("APIGetData\\GetElectricityProductionAPI.exe");
+    }
     
+    printf("Reading file: \n");
+
     if (readFile()) {
-        printf("NEJ\n");
+        printf("Error 404: file not found.\n");
     }
     return(0);
 }
 
 int readFile(void) {
+    int evArrayLen = 17000;
+    double* evArray = (double*)malloc(evArrayLen*sizeof(double)); 
+    int batCapacity = 50;
+
     FILE *fpointer;
-    double price;
+    double pro_ex = 0;
+    int numOfEvCharging = 0;
+    double pro_exPlusCharging = 0;
     char date[20];
     char singleline[20];
+    double kat = 0;
+    double proArray[400];
+    double limit = 0;
     fpointer = fopen("output.txt", "r"); /* For at fortælle at vi vil gerne læse fra det fil, vi bruger derfor "r" */
-    
+    generateEvArray(evArray, evArrayLen);
+    qsort(evArray, evArrayLen, sizeof(double), cmpFun);
+    int i = 0;
+
     if (fpointer != NULL) {
-        int i = 0;
         while (!feof(fpointer)){
             for(int x = 0; x < 2; x++) {
                 fgets(singleline, 20, fpointer); /* For at læse den linje pr. linje */
                 if (x % 2 == 0) 
-                    memcpy(date, strtok(singleline, "\n"), sizeof singleline);
-                else 
-                    price = atof(singleline); /* Konverterer til en double */
-            } 
-            i++;
+                    memcpy(date, strtok(singleline, "\n"), sizeof(singleline));
+                else {
+                    kat = atof(singleline); /* Konverterer til en double !!HELLO!! */
+                    pro_ex = kat;
+                    /*
+                    proArray[i] = kat;
+                    i++;
+                    */
+                    //pro_ex = kat; 
+                }
+            }
+
+            pro_exPlusCharging = getEvCharging(evArray, evArrayLen, pro_ex, &numOfEvCharging, limit);
+            double sum = 0;
+            for(int i = 0; i < evArrayLen; i++) {
+                sum += evArray[i];
+            }
+            printf("Ev charging now: %d Time: %s | pro: %f | proG: %f | sum: %f \n", numOfEvCharging, date, pro_exPlusCharging, pro_ex, sum); 
         }
+        /*
+        qsort(proArray, 400, sizeof(double), cmpFun);
+        limit = proArray[50];
+
+        //printArray(proArray, 400);
+        printf("limit: %f", limit);
+        */
     }
     else 
         return 1;
+    
+    free(evArray);
+
     return fclose(fpointer);
 }
+
+double getEvCharging(double evArray[], int evArrayLen, double pro_ex, int* numOfEvCharging, double limit) {
+    int pro_exTing = 0;
+    int chargeRate = 11;
+    
+    *numOfEvCharging = 0;
+    int i = 1;
+    while (pro_ex+pro_exTing <limit && i < evArrayLen) {
+        //printf(" [%f] ", pro_exTing);
+        evArray[i] += 0.000916;
+        ++(*numOfEvCharging);
+        pro_exTing = (0.000916)*i;
+        i++;
+    }
+    //printf(" {{{%f}} ",pro_ex+pro_exTing);
+
+    return pro_ex+pro_exTing;
+}
+
+void generateEvArray(double evArray[], int evArrayLen){
+    for (int i = 0; i < evArrayLen; i++){
+        evArray[i] = rand() % (100-10) + 10;
+    }
+}
+
+int cmpFun(const void *ep1, const void *ep2) {
+    double *tp1 = (double *)ep1,
+          *tp2 = (double *)ep2;
+    
+    if(*tp1 < *tp2) 
+        return -1;
+    else if (*tp1 >*tp2)
+        return 1;
+    else
+        return 0;
+}
+
+void printArray(double array[], int arrayLen) {
+    for(int i = 0; i < arrayLen; i++ ) {
+        printf(" %f ", array[i]);
+    }
+}
+
+
