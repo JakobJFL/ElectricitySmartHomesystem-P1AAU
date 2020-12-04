ec65c6849d7d1466b993098bf4401e4def355dc5
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2020-12-04 15:32:39 +0100

    fix all

--- electricVehicle.h
+++ electricVehicle.h
@@ -18,18 +18,12 @@ typedef struct {
 
 enum modelType {tesla, huyndai, renault, vw};
 
-double sumOfbatCharged(electricVehicle evArray[], int evArrayLen);
-
-void generateEvArray(electricVehicle evArray[], int evArrayLen);
-void generateTeslaArray(electricVehicle evArray[], int evArrayStart, int evArrayEnd);
-void generateHyundaiArray(electricVehicle evArray[], int evArrayStart, int evArrayEnd);
-void generateRenaultArray(electricVehicle evArray[], int evArrayStart, int evArrayEnd);
-void generateGolfArray(electricVehicle evArray[], int evArrayStart, int evArrayEnd);
-int chargeEvOneHour(electricVehicle evArray[], int evArrayLen, int evToCharge, float* evCharge);
-void generateCustomArray(electricVehicle evArray[], int evArrayStart, int evArrayEnd, enum modelType mType);
-electricVehicle makeEv(float capacity, float chargerate, float charge, char modelName[MODEL_NAME_LEN]);
-void printEV(electricVehicle array[], int arrayLength);
-void chargeEV(electricVehicle evArray[], int evArrayLen, spotPrices elPrArray[]);
+void printAndchargeEV(spotPrices elPrArray[]);
 void readFileEV(electricVehicle array[], int arrayLen);
 int getSumOfEvs(electricVehicle array[], int arrayLen);
 void setEvArrayValues(electricVehicle arrayFile[], int arrayFileLen, electricVehicle evArray[], int evArrayLen);
+void setBatteryCharge(electricVehicle evArray[], int evArrayLen);
+void printEV(electricVehicle array[], int arrayLength);
+void chargeEV(electricVehicle evArray[], int evArrayLen, spotPrices elPrArray[]);
+double sumOfbatCharged(electricVehicle evArray[], int evArrayLen);
+int chargeEvOneHour(electricVehicle evArray[], int evArrayLen, int evToCharge, float* evCharge);
